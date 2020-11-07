cd4afd49068d68d791c8787a8982b84d63d6bf08
Syméon Smith <symeon.smith@gmail.com>
2020-11-07 19:04:58 +0100

    Remove personal email

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config():
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['symeon.smith@gmail.com']
+    ADMINS = ['test@mail.com']
     POSTS_PER_PAGE = 10
     # available: mp, identicon, monsterid, wavatar, retro, robohash, blank
     AVATAR_STYLE = 'retro'
